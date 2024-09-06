d3301aff8ce76e151507fc01c3ba70a384ebdf67
Alex Wilkom <alex.wilkom@icloud.com>
2024-09-06 07:47:12 +0200

    Start over the logic

--- script.js
+++ script.js
@@ -27,26 +27,53 @@ const percentBtn = document.querySelector(".percent");
 const decimal = document.querySelector(".decimal");
 const operands = document.querySelectorAll(".operand");
 const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals");
 
 let leftOperand = "";
 let rightOperand = "";
-let operator = "";
+let currentOperator = "";
 
 function displayText(value) {
-    const displayText = display.textContent;
-    if (displayText !== "0") {
+    if (!leftOperand || currentOperator) {
+        display.textContent = "";
         display.textContent += value;
     } else {
-        display.textContent = "";
-        display.textContent = value;
+        display.textContent += value;
     }
+}
+
+function operate(operator, leftOperand, rightOperand) {
+    const result = operations[operator](Number(leftOperand), Number(rightOperand));
+    displayText(String(result));
+    leftOperand = String(result);
+    rightOperand = "";
 }
 
 operands.forEach(operand => {
     operand.addEventListener("click", (event) => {
         const value = event.target.textContent;
         displayText(value);
-        if (!operator) leftOperand += value
-        else rightOperand += value
+        if (currentOperator) rightOperand += value;
+        else leftOperand += value;
     })
+})
+
+operators.forEach(operator => {
+    operator.addEventListener("click", (event) => {
+        const newOperator = event.target.textContent;
+
+        if (leftOperand && rightOperand) {
+            operate(currentOperator, leftOperand, rightOperand);
+            currentOperator = newOperator;
+        } else if (!rightOperand) {
+            currentOperator = newOperator;
+        }
+    })
+})
+
+equals.addEventListener("click", () => {
+    if (currentOperator) {
+        operate(currentOperator, leftOperand, rightOperand);
+        currentOperator = "";
+    }
 })
